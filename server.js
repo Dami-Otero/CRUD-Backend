14a709cc3a32e7c085ef6f6d54d2c9b8a54f35b0
MZ - MacBook14 <marcozamponi17@gmail.com>
2024-06-12 16:28:56 +0200

    new items in db.json

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ const router = jsonServer.router('db.json')
 const middlwares = jsonServer.defaults()
 const cors = require('cors')
 
-server.use(cors())
+server.use(cors({
+    origin: 'http://localhost:4200/',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+
+}))
+
+server.use(function(req, res, next){
+    setTimeout(next, 10000);
+  });
 
 server.use(middlwares)
 server.use(router)
-
-server.use(function(req, res, next){
-    //setTimeout(next, 10000);
-    next()
-  });
-
 
 const port = process.env.PORT || 3000
 
