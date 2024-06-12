dbdd489310fda9ebda5368b6c7559623879caa31
Dami-Otero <damiango.lucas@gmail.com>
2024-06-12 16:00:25 +0200

    Minor changes

--- server.js
+++ server.js
@@ -4,19 +4,16 @@ const router = jsonServer.router('db.json')
 const middlwares = jsonServer.defaults()
 const cors = require('cors')
 
-server.use(cors({
-    origin: 'http://localhost:4200/',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-
-}))
-
-server.use(function(req, res, next){
-    setTimeout(next, 10000);
-  });
+server.use(cors())
 
 server.use(middlwares)
 server.use(router)
+
+server.use(function(req, res, next){
+    //setTimeout(next, 10000);
+    next()
+  });
+
 
 const port = process.env.PORT || 3000
 
