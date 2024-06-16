837ff81a22042a8afda903c00e8130ce524050fe
Dami-Otero <damiango.lucas@gmail.com>
2024-06-17 00:21:21 +0200

    16/6

--- server.js
+++ server.js
@@ -4,19 +4,18 @@ const router = jsonServer.router('db.json')
 const middlwares = jsonServer.defaults()
 const cors = require('cors')
 
-server.use(cors())
+server.use(cors({
+    origin: 'http://localhost:4200/',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
+server.use(function(req, res, next){
+    setTimeout(next, 10000);
+  });
 
 server.use(middlwares)
 server.use(router)
 
-server.use(function(req, res, next){
-    //setTimeout(next, 10000);
-    next()
-  });
-
-
 const port = process.env.PORT || 3000
 
-server.listen(port, () => {
-    console.log(`JSON Server is running on port ${port}`)
-})
